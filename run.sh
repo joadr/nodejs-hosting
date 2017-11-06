ace5b7d5f28b2df231e14fbd342f7ff1c1c733df
Joaquin Diaz <joaquin.diaz@ies.cl>
2017-11-06 17:37:33 -0300

    show me

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
 export $(cat /home/deploy/.env | xargs)
 
+echo $(ls)
+
+echo $(ls /home/deploy/)
+
+echo $(ls /home/deploy/script)
+
+echo $(ls /home/deploy/code/app)
+
 node /home/deploy/script/service/lib/index.js | sudo tee /home/deploy/app.log
