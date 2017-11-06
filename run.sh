8e35ed23e49e6a578197bf1539e99c0d7115b28d
Joaquin Diaz <joaquin.diaz@ies.cl>
2017-11-06 17:43:26 -0300

    advancing...

--- run.sh
+++ run.sh
@@ -1,11 +1,9 @@
 export $(cat /home/deploy/.env | xargs)
 
-echo $(ls)
-
 echo $(ls /home/deploy/)
 
-echo $(ls /home/deploy/script)
+echo $(ls /home/deploy/app)
 
-echo $(ls /home/deploy/code/app)
+echo $(ls /home/deploy/app/service)
 
 node /home/deploy/script/service/lib/index.js | sudo tee /home/deploy/app.log
