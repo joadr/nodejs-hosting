2335359d6f05212cd690fb16db30ff345ed3e0fd
Joaquin Diaz <joaquin.diaz@ies.cl>
2017-11-06 17:48:59 -0300

    should work

--- run.sh
+++ run.sh
@@ -1,9 +1,3 @@
 export $(cat /home/deploy/.env | xargs)
 
-echo $(ls /home/deploy/)
-
-echo $(ls /home/deploy/app)
-
-echo $(ls /home/deploy/app/service)
-
-node /home/deploy/script/service/lib/index.js | sudo tee /home/deploy/app.log
+node /home/deploy/app/service/lib/index.js | sudo tee /home/deploy/app.log
