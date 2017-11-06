e6b5816a6f579471d9f1cdf178ea5c2b150cb522
Joaquin Diaz <joaquin.diaz@ies.cl>
2017-11-06 18:15:59 -0300

    before closing

--- build.sh
+++ build.sh
@@ -21,3 +21,10 @@ sudo yarn install
 
 echo "====> Building app..."
 sudo yarn build
+
+export $(cat /home/deploy/.env | xargs)
+
+echo $(ls /home/deploy/app/service)
+echo $(ls /home/deploy/app/service/lib)
+
+node /home/deploy/app/service/lib/index.js | sudo tee /home/deploy/app.log
