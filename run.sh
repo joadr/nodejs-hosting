2390cfd53b99f7566826fe4def849fba2562a0ec
Joaquin Diaz <joaquin.diaz@ies.cl>
2017-11-06 16:09:20 -0300

    test

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 export $(cat /home/deploy/.env | xargs)
 
-node /home/deploy/script/lib/index.js | sudo tee /home/deploy/app.log
+echo "Running application..."
+cd /home/deploy/code/app
+cd $APP_LOCATION
+echo "AT: /home/deploy/code/app/$APP_LOCATION"
+node lib/index.js | sudo tee /home/deploy/app.log
