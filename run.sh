6fd01efe7a2a1c74fa25979a5a1b5909c8f39ede
Joaquin Diaz <joaquin.diaz@ies.cl>
2017-11-06 16:52:33 -0300

    probando

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 export $(cat /home/deploy/.env | xargs)
 
 echo "Running application..."
-cd /home/deploy/code/app
-cd $APP_LOCATION
-echo "AT: /home/deploy/code/app/$APP_LOCATION"
-node lib/index.js | sudo tee /home/deploy/app.log
+echo "$MONGO_URL" > /home/deploy/app.log
+node /home/deploy/code/app/$APP_LOCATION/lib/index.js | sudo tee /home/deploy/app.log
