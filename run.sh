11d5095a729d68e81e21cffe5675369bf34657cf
Joaquin Diaz <joaquin.diaz@ies.cl>
2017-11-06 18:11:53 -0300

    asd

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 export $(cat /home/deploy/.env | xargs)
 
+echo $(ls /home/deploy/app/service)
+echo $(ls /home/deploy/app/service/lib)
+
 node /home/deploy/app/service/lib/index.js | sudo tee /home/deploy/app.log
